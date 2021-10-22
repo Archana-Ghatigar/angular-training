5c663c2816d4b4fe8a95316f5e660af80ecb9b07
Archana-Ghatigar <31721864+Archana-Ghatigar@users.noreply.github.com>
2021-10-22 14:29:59 +0530

    Add files via upload

---
+++ src/app/AmazonData.service.ts
@@ -0,0 +1,30 @@
+import { LoggerService } from './logger.service';
+import { ProductsdataService } from './productsdata.service';
+import { Inject, Injectable } from '@angular/core';
+import { Product } from './productlist/product';
+
+@Injectable({
+  providedIn:'root'
+})
+export class AmazonDataService extends ProductsdataService{
+  //private productList;
+  constructor(private logger:LoggerService) {
+    super();
+    console.log('AmazonDataService invoked');
+    let p1 = new Product(1, "Angular CookBook", "Technical", 240, "assets/images/angular.jpg");
+    let p2 = new Product(2, "Ajax for Dummies", "Technical", 640, "assets/images/ajax.jpg");
+    let p3 = new Product(3, "Life Rules", "Self help", 540, "assets/images/liferules.jpg");
+    let p4 = new Product(4, "Tinkle", "Comic", 140, "assets/images/tinkle.jpg");
+    let p5 = new Product(5, "You can Win", "Self help", 440, "assets/images/youcanwin.jpg");
+    this.productList.push(p1);
+    this.productList.push(p2);
+    this.productList.push(p3);
+    this.productList.push(p4);
+    this.productList.push(p5);
+    this.logger.log('Data inititalized in AmazonDataService');
+  }
+  getProductList():Array<Product>{
+    return this.productList;
+  }
+
+}
---
+++ src/app/BigBasketDataService.ts
@@ -0,0 +1,30 @@
+import { ProductsdataService } from './productsdata.service';
+import { Product } from './productlist/product';
+import { LoggerService } from './logger.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn:'root'
+})
+export class BigBasketDataService extends ProductsdataService{
+  
+  constructor(private logger:LoggerService) {  
+    super();
+    let p1 = new Product(1, "MotoG4", "Mobiles", 24000, "assets/images/1.jpg");
+    let p2 = new Product(2, "SonyDSC", "Camera", 64000, "assets/images/2.jpg");
+    let p3 = new Product(3, "Lenovo110", "Laptops", 54000, "assets/images/3.jpg");
+    let p4 = new Product(4, "Xiomi55", "Mobiles", 14000, "assets/images/4.jpg");
+    let p5 = new Product(5, "Samsung Galaxy", "Mobiles", 44000, "assets/images/5.jpg");
+    this.productList.push(p1);
+    this.productList.push(p2);
+    this.productList.push(p3);
+    this.productList.push(p4);
+    this.productList.push(p5);
+    this.logger.log('Data inititalized in BigBasketDataService');
+
+  }
+  getProductList():Array<Product>{
+    return this.productList;
+  }
+
+}
